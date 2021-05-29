b008b6f21256740d42575305d08ccaf15ecdafc6
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-05-29 16:15:49 +0700

    edit pgSQL

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => 'postgres://nqdgbqtnltnfpl:dfa252247fc98a9f3664b6f2fe23ebc5db4884bb1bd07709467e8b27b9a9b552@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d5gb4taam1vf31',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
